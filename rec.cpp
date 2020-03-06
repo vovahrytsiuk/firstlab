1888b3a35b2cd3ade27e159bf8b0d949ccb89e6e
vovahrytsiuk <vovaoffonua@gmail.com>
2020-03-06 15:56:02 +0200

    string invert

--- rec.cpp
+++ rec.cpp
@@ -22,3 +22,50 @@ country invert_score(string record){
     }
     return decrupt;
 }
+
+vector<country> decrypting(vector<string> input){
+    vector<country> inputData;
+    for(int i = 0; i < input.size();i++){
+        inputData.push_back(invert_score(input[i]));
+    }
+    return inputData;
+}
+
+void calculate_sum(country& Country){
+    Country.sum = 0;
+    for(int i = 0; i < Country.score.size(); i++){
+        Country.sum += Country.score[i];
+    }
+}
+
+void sum_fo_all(vector<country>& input){
+    for(int i = 0; i< input.size(); i++){
+        calculate_sum(input[i]);
+    }
+}
+
+void sort_result(vector<country>&input){
+    for(int i = 0; i < input.size();i++){
+        for(int j = i; j < input.size(); j++){
+            if(input[i].sum < input[j].sum){
+                country temp = input[i];
+                input[i] = input[j];
+                input[j] = temp;
+            }
+            else if(input[i].sum == input[j].sum && input[i].country_name < input[j].country_name){
+                country temp = input[i];
+                input[i] = input[j];
+                input[j] = temp;
+            }
+        }
+    }
+}
+
+void determine_marks(vector<country>&input){
+    input[0].mark = 12;
+    input[1].mark = 10;
+    for(int i = 2; i < input.size();i++){
+        input[i].mark = 1;
+    }
+}
+
--- rec.h
+++ rec.h
@@ -3,17 +3,26 @@
 
 #include <string>
 #include <vector>
+#include <experimental/filesystem>
+
 
 using namespace std;
+using namespace std::experimental::filesystem;
 
 struct country{
     string country_name;
     vector <int> score;
+    long sum;
+    int mark;
 };
 
 country invert_score(string record);
-
-
+vector<country> decrypting(vector<string> input);
+void calculate_sum(country& Country);
+void sum_fo_all(vector<country>& input);
+void sort_result(vector<country>&input);
+void determine_marks(vector<country>&input);
+vector<path> 
 
 
 #endif
