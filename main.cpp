155a4315988cba06255756025a4dcf31ee2f5a57
vovahrytsiuk <vovaoffonua@gmail.com>
2020-03-07 14:47:00 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,19 @@ using namespace std;
 
 
 int main(){
-	
+	string directory;
+  getline(cin, directory);
+  vector<path> files = input(directory);
+  vector<string> countryName;
+  vector<vector<int>> scores;
+  get_info(files, countryName, scores);
+  int** mark = calculate_mark(scores);
+  int n = scores.size();
+  int m = scores[0].size();
+  int* sum = sums(mark, n, m);
+  sort(countryName, sum, n);
+  print_top(countryName, sum);
+
+  
 	return 0;
 }
