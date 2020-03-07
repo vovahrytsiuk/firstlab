578b9c42533c80b3a25a6b094f33c227c8b9193a
vovahrytsiuk <vovaoffonua@gmail.com>
2020-03-07 14:43:24 +0200

    file

--- rec.cpp
+++ rec.cpp
@@ -1,71 +1,51 @@
 #include "rec.h"
 
-country invert_score(string record){
-    country decrupt;//contry and its score int record
-    string tempName;
-    int i = 0; 
-    while(record[i] != ','){
-        tempName+=record[i];
-        i++;
-    }
-    decrupt.country_name = tempName;
-    i++;
-    string markStr = "";
-    for(; i < record.length();i++){
-        if(record[i] == ',' || i == record.length()-1){
-            decrupt.score.push_back(stoi(markStr));
-            markStr = "";
-        }
-        else{
-            markStr += record[i];
-        }
-    }
-    return decrupt;
+vector<path> input(const string directory) {
+  vector<path>files;
+  const directory_iterator begin(directory);
+  const directory_iterator end;
+  copy_if(begin, end, back_inserter(files),
+    [](const path& p) {
+      return is_regular_file(p) && p.extension() == ".csv";
+    });
+  return files;
 }
 
-vector<country> decrypting(vector<string> input){
-    vector<country> inputData;
-    for(int i = 0; i < input.size();i++){
-        inputData.push_back(invert_score(input[i]));
-    }
-    return inputData;
+void get_info(vector<path> files, vector<string>& countryName, vector<vector<int>>& scores) {
+  for (auto& f : files) {
+    input_data(f, countryName, scores);
+  }
 }
 
-void calculate_sum(country& Country){
-    Country.sum = 0;
-    for(int i = 0; i < Country.score.size(); i++){
-        Country.sum += Country.score[i];
-    }
+void input_data(path file, vector<string>& countryName, vector<vector<int>>& scores) {
+  fstream f(file);
+  int n;
+  f >> n;
+  string str;
+  getline(f, str);
+  for (int i = 0; i < n; i++) {
+    getline(f, str);
+    parsing(str, countryName, scores[i]);
+  }
 }
 
-void sum_fo_all(vector<country>& input){
-    for(int i = 0; i< input.size(); i++){
-        calculate_sum(input[i]);
+void parsing(string line, vector<string>& countryName, vector<int>& score) {
+  string temp = "";
+  int iter = 0;
+  while (line[iter] != ',') {
+    temp += line[iter];
+    iter++;
+  }
+  iter++;
+  countryName.push_back(temp);
+  temp = "";
+  for (; iter < line.length(); iter++) {
+    if (line[iter] == ',') {
+      score.push_back(stoi(temp));
+      temp = "";
     }
-}
-
-void sort_result(vector<country>&input){
-    for(int i = 0; i < input.size();i++){
-        for(int j = i; j < input.size(); j++){
-            if(input[i].sum < input[j].sum){
-                country temp = input[i];
-                input[i] = input[j];
-                input[j] = temp;
-            }
-            else if(input[i].sum == input[j].sum && input[i].country_name < input[j].country_name){
-                country temp = input[i];
-                input[i] = input[j];
-                input[j] = temp;
-            }
-        }
+    else {
+      temp += line[iter];
     }
+  }
 }
-
-void determine_marks(vector<country>&input){
-    input[0].mark = 12;
-    input[1].mark = 10;
-    for(int i = 2; i < input.size();i++){
-        input[i].mark = 1;
-    }
-}
-
--- rec.h
+++ rec.h
@@ -1,28 +1,17 @@
 #ifndef REC_H
 #define REC_H
 
-#include <string>
-#include <vector>
-#include <experimental/filesystem>
 
+
+#include <experimental/filesystem>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 using namespace std::experimental::filesystem;
-
-struct country{
-    string country_name;
-    vector <int> score;
-    long sum;
-    int mark;
-};
-
-country invert_score(string record);
-vector<country> decrypting(vector<string> input);
-void calculate_sum(country& Country);
-void sum_fo_all(vector<country>& input);
-void sort_result(vector<country>&input);
-void determine_marks(vector<country>&input);
-vector<path> 
 
 
 #endif
